a7bc67c4cf631535f07c96a4242ab2c11fdb49fe
Jiyong Park <jiyong@google.com>
2019-04-15 14:19:03 +0900

    renoir: Add updatable_apex.mk
    The product abstracts configs that need to be set to support updating of APEXes.
    
    Change-Id: Id5aa944ceba6a6846308f1c18a08af8b3bb38589

--- device.mk
+++ device.mk
@@ -7,6 +7,9 @@
 PRODUCT_BUILD_SUPER_PARTITION := false
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
