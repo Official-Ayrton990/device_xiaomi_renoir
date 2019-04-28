003592c468c1682edac2266f68e9e8903c78c03c
XiNGRZ <chenxingyu92@gmail.com>
2019-04-29 03:23:26 +0800

    renoir: Ship and update vbmeta/dtbo images

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -70,6 +70,9 @@ TARGET_RECOVERY_PIXEL_FORMAT := "BGRA_8888"
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USERIMAGES_USE_F2FS := true
 
+# Releasetools
+TARGET_RELEASETOOLS_EXTENSIONS := $(DEVICE_PATH)
+
 # Sepolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
 
