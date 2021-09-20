8d36ba9b74ba6d75a2356a72bb95589ea1c84f5c
Cosmin Tanislav <demonsingur@gmail.com>
2021-09-21 01:58:57 +0300

    renoir: sensors: avoid target name conflicts

--- device.mk
+++ device.mk
@@ -332,5 +332,5 @@ PRODUCT_COPY_FILES += \
 
 # Sensors
 PRODUCT_PACKAGES += \
-    android.hardware.sensors@2.1-service.multihal \
+    android.hardware.sensors@2.1-service.xiaomi_holi-multihal \
     libsensorndkbridge
