16d6bdc1562ec4b78c9d48a336b50ccbbcae3b73
Arian <arian.kulmer@web.de>
2021-11-05 16:44:55 +0100

    renoir: Add power HAL extension and use xiaomi-touch to handle dt2w
    Change-Id: I5c65e0df255853eb2b3018a27ceb807c111bc4df

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -164,6 +164,9 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 # Platform
 TARGET_BOARD_PLATFORM := lahaina
 
+# Power
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
+
 # Properties
 TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
