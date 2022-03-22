e6029a72cf1e0046db7c994bf5287c8c3c643972
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2022-03-23 05:01:44 +0530

    renoir: Import compatibility matrix and manifest
    from renoir_global-user 11 RKQ1.201112.002 V12.5.7.0.RKIMIXM release-keys

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -28,6 +28,10 @@ TARGET_OTA_ASSERT_DEVICE := renoir
 TARGET_BOOTLOADER_BOARD_NAME := lahaina
 TARGET_NO_BOOTLOADER := true
 
+# HIDL
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE := $(DEVICE_PATH)/manifest.xml
+
 # Kernel
 BOARD_KERNEL_CMDLINE := console=ttyMSM0,115200n8 androidboot.hardware=qcom androidboot.console=ttyMSM0 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 loop.max_part=7 cgroup.memory=nokmem,nosocket reboot=panic_warm
 BOARD_KERNEL_IMAGE_NAME := Image
