1d2f5b401313722dafaa9632cf7556cc3c24ce92
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2022-03-23 05:08:15 +0530

    renoir: don't build super partition and use dynamic partitions
    Signed-off-by: Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>

--- device.mk
+++ device.mk
@@ -4,6 +4,9 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
