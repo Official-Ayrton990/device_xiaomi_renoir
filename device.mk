ede657a5c1e746d68de1652bc939e59de79de798
Ramii Ahmed <ramy@ahmedramy.com>
2022-04-04 00:16:58 +0530

    renoir: add AVB keys packages
    Change-Id: I8025d203add4629269bb37460d45288bc264b2fd

--- device.mk
+++ device.mk
@@ -165,6 +165,13 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml
 
+# AVB
+PRODUCT_PACKAGES += \
+    q-gsi.avbpubkey \
+    r-gsi.avbpubkey \
+    s-gsi.avbpubkey
+
+
 # Boot animation
 TARGET_SCREEN_HEIGHT := 2400
 TARGET_SCREEN_WIDTH := 1080
