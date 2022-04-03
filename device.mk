71f9e44738b92068aad180adb3f2592291a0ceae
Cosmin Tanislav <demonsingur@gmail.com>
2022-04-04 00:23:17 +0530

    renoir: build vndservicemanager
    * vndservicemanager has been removed from API30 devices (aosp/1235751)
    * but we still need it for display services.
    
    Change-Id: I3159d54219986566898f8b3895bc69da26267a8e

--- device.mk
+++ device.mk
@@ -229,7 +229,8 @@ PRODUCT_PACKAGES += \
     vendor.qti.hardware.display.mapper@1.1.vendor \
     vendor.qti.hardware.display.mapper@2.0.vendor \
     vendor.qti.hardware.display.mapper@3.0.vendor \
-    vendor.qti.hardware.display.mapper@4.0.vendor
+    vendor.qti.hardware.display.mapper@4.0.vendor \
+    vndservicemanager
 
 # DRM
 PRODUCT_PACKAGES += \
