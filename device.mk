efd701ef70478902b8ced6acdc5dd7d833a77d10
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2022-04-05 07:41:44 +0530

    renoir: build IPACM

--- device.mk
+++ device.mk
@@ -263,6 +263,13 @@ PRODUCT_PACKAGES += \
     libhwbinder \
     libhwbinder.vendor
 
+# IPACM
+PRODUCT_PACKAGES += \
+    ipacm \
+    IPACM_cfg.xml \
+    libipanat \
+    liboffloadhal
+
 # NFC
 PRODUCT_SOONG_NAMESPACES += \
     vendor/nxp/opensource/sn100x
