1d1434ff3b8835553a07647be2cec08303745572
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2022-04-17 00:30:58 +0530

    renoir: import powerhint

--- device.mk
+++ device.mk
@@ -324,6 +324,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service-qti \
     vendor.qti.hardware.perf@2.2.vendor
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml
+
 # RIL
 PRODUCT_PACKAGES += \
     libjson \
