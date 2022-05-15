fb703ac82f9160897176fdbd91333c620fdc7593
Albert Wang <albertccwang@google.com>
2022-05-16 05:28:08 +0530

    renoir: Update USB HAL to V1.3 implementation
    Supports to enable/disable USB data signaling
    
    Bug: 161414036
    Test: Pass USB V1.3 HIDL tests
    Signed-off-by: Albert Wang <albertccwang@google.com>
    Change-Id: Iffe00f8753206fb66cd3ab96cae5aa5ad9c410cd

--- device.mk
+++ device.mk
@@ -490,7 +490,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.2-service-qti
+    android.hardware.usb@1.3-service-qti
 
 PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
