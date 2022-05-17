a5f71f4205b5739ee24fbc1f479dc96a4f72122a
Carlos Ayrton Lopez Arroyo <15030201@itcelaya.edu.mx>
2022-05-18 04:23:02 +0530

    renoir: build gatekeeper
    Solves the following issue: CANNOT LINK EXECUTABLE "/vendor/bin/hw/android.hardware.gatekeeper@1.0-service-qti": library "android.hardware.gatekeeper@1.0.so"

--- device.mk
+++ device.mk
@@ -271,6 +271,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RECOVERY)/root/first_stage_ramdisk/fstab.qcom \
     $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_VENDOR_RAMDISK)/first_stage_ramdisk/fstab.qcom
 
+# Gatekeeper
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0.vendor
+
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
